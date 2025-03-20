31817eb1c9536bebbeefb7c4f0d8ca7105542ffd
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2025-03-20 15:11:05 +0900

    [Silver III] Title: 1로 만들기, Time: 336 ms, Memory: 40792 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,14 +1,22 @@
 import sys
 
 input = sys.stdin.readline
-n = int(input())
-dp = [0] * (n + 1)
 
-for i in range(2, n + 1):
-    if i % 3 == 0:
-        dp[i] = min(dp[i // 3] + 1, dp[i]) if dp[i] != 0 else dp[i // 3] + 1
-    if i % 2 == 0:
-        dp[i] = min(dp[i // 2] + 1, dp[i]) if dp[i] != 0 else dp[i // 2] + 1
-    dp[i] = min(dp[i - 1] + 1, dp[i]) if dp[i] != 0 else dp[i - 1] + 1
 
-print(dp[n])
+def main():
+    x = int(input())
+    dp = [0x4F4F4F4F for i in range(x + 1)]
+    dp[1] = 0
+    for i in range(1, x + 1):
+        if i % 3 == 0:
+            dp[i] = min(dp[i], dp[i // 3] + 1)
+        if i % 2 == 0:
+            dp[i] = min(dp[i], dp[i // 2] + 1)
+        if i > 1:
+            dp[i] = min(dp[i], dp[i - 1] + 1)
+
+    print(dp[x])
+
+
+if __name__ == "__main__":
+    main()
