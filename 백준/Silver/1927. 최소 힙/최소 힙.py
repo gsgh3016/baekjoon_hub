470dfcbacca2607a772ef8a7b28d521b086cddf9
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2025-03-26 18:52:41 +0900

    [Silver II] Title: 최소 힙, Time: 92 ms, Memory: 38340 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.py
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -2,15 +2,21 @@ import heapq
 import sys
 
 input = sys.stdin.readline
-n = int(input())
-hq = []
-heapq.heapify(hq)
-for _ in range(n):
-    num = int(input())
-    if num == 0:
-        if not hq:
+
+
+def main():
+    n = int(input())
+    h = []
+    heapq.heapify(h)
+    for _ in range(n):
+        num = int(input())
+        if num == 0 and len(h) == 0:
             print(0)
+        elif num == 0:
+            print(heapq.heappop(h))
         else:
-            print(heapq.heappop(hq))
-    else:
-        heapq.heappush(hq, num)
+            heapq.heappush(h, num)
+
+
+if __name__ == "__main__":
+    main()
