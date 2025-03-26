eb7f88e65c48c2d85cbc2c14d677b7780d00f237
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2025-03-26 18:48:09 +0900

    [Silver IV] Title: 듣보잡, Time: 68 ms, Memory: 44192 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,16 +1,22 @@
 import sys
 
 input = sys.stdin.readline
-n, m = map(int, input().split())
-s1, s2 = set(), set()
 
-for _ in range(n):
-    s1.add(input().rstrip())
 
-for _ in range(m):
-    s2.add(input().rstrip())
+def main():
+    n, m = map(int, input().split())
+    s1, s2 = set(), set()
+    for _ in range(n):
+        s1.add(input().rstrip())
+    for _ in range(m):
+        s2.add(input().rstrip())
+    inter = list(s1.intersection(s2))
+    inter.sort()
 
-inter = sorted(list(s1.intersection(s2)))
-print(len(inter))
-for name in inter:
-    print(name)
+    print(len(inter))
+    for name in inter:
+        print(name)
+
+
+if __name__ == "__main__":
+    main()
